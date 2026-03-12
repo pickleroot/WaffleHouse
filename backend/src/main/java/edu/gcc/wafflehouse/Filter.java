dcd604a8109a5da430266f4a263deb7758f90f3f
Ina Tang <inatang75@outlook.com>
2026-03-12 00:38:24 -0400

    Update Filter to save user input

--- backend/src/main/java/edu/gcc/wafflehouse/Filter.java
+++ backend/src/main/java/edu/gcc/wafflehouse/Filter.java
@@ -4,8 +4,17 @@ import java.util.ArrayList;
 /**
  * Abstract class for all filters to extend
  * @author Ina Tang (Last edit)
- * @param <T> Type for pattern. E.g. String for CourseNameFilter, Integer for CreditHourFilter, Professor for ProfessorFilter
  */
-public abstract class Filter<T> {
-    public abstract ArrayList<Course> apply(ArrayList<Course> courses, T pattern);
+public abstract class Filter {
+    protected Object input;
+
+    public Filter(Object input) {
+        this.input = input;
+    }
+
+    public Object getInput() { return input; }
+
+    public void setInput(Object input) { this.input = input; }
+
+    public abstract ArrayList<Course> apply(ArrayList<Course> courses);
 }
