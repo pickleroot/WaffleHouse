27984f7f383404a67806908f2c35fc0e4511385f
Ina Tang <inatang75@outlook.com>
2026-04-28 09:44:46 -0400

    fix: improve filter combobox behavior

--- frontend/src/components/ui/combobox.tsx
+++ frontend/src/components/ui/combobox.tsx
@@ -96,11 +96,12 @@ function ComboboxContent({
   align = "start",
   alignOffset = 0,
   anchor,
+  collisionAvoidance,
   ...props
 }: ComboboxPrimitive.Popup.Props &
   Pick<
     ComboboxPrimitive.Positioner.Props,
-    "side" | "align" | "sideOffset" | "alignOffset" | "anchor"
+    "side" | "align" | "sideOffset" | "alignOffset" | "anchor" | "collisionAvoidance"
   >) {
   return (
     <ComboboxPrimitive.Portal>
@@ -110,6 +111,7 @@ function ComboboxContent({
         align={align}
         alignOffset={alignOffset}
         anchor={anchor}
+        collisionAvoidance={collisionAvoidance}
         className="isolate z-50"
       >
         <ComboboxPrimitive.Popup
@@ -274,7 +276,6 @@ function ComboboxChip({
 
 function ComboboxChipsInput({
   className,
-  children,
   ...props
 }: ComboboxPrimitive.Input.Props) {
   return (
